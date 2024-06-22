04c535b132ac476f371cd96d4d9f5e6ded0928c1
Varga Barnabas <barnabas_varga@mckinsey.com>
2024-06-23 00:33:38 +0200

    fix: mobile truncate fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </head>
       <body
         className={clsx(
           "flex flex-col items-center",
--- components/restaurant-card/restaurant-card.tsx
+++ components/restaurant-card/restaurant-card.tsx
@@ -15,39 +15,38 @@ export default function RestaurantCard({ restaurant }: Props) {
         ""
       )}`}
       target="_blank"
+      className="relative flex h-max w-full  cursor-pointer flex-col rounded-lg bg-white shadow hover:shadow-md transition-all duration-300 ease-out hover:scale-[103%]"
     >
-      <div className="relative flex h-max w-full min-w-0 max-w-full cursor-pointer flex-col rounded-lg bg-white shadow hover:shadow-md transition-all duration-300 ease-out hover:scale-[103%]">
-        <div className="relative aspect-wolt overflow-hidden rounded-t-lg">
-          <Image
-            src={restaurant.image.url}
-            layout="fill"
-            alt=""
-            objectFit="cover"
-            priority
-          />
-          {!restaurant.venue.online ? (
-            <div className="absolute inset-0 flex items-center justify-center bg-light-gray/[56%]">
-              <p className="text-lg text-white">Closed</p>
-            </div>
-          ) : null}
-        </div>
-        <div className="flex w-full justify-between gap-x-4 border-b border-dashed p-4">
-          <div className="w-[80%]">
-            <p className="truncate text-lg font-medium">{restaurant.title}</p>
-            <p className="min-w-0 truncate text-sm opacity-[64%] ">
-              {restaurant.venue.short_description}
-            </p>
+      <div className="flex relative aspect-wolt overflow-hidden rounded-t-lg w-full">
+        <Image
+          src={restaurant.image.url}
+          layout="fill"
+          alt=""
+          objectFit="cover"
+          priority
+        />
+        {!restaurant.venue.online ? (
+          <div className="absolute inset-0 flex items-center justify-center bg-light-gray/[56%]">
+            <p className="text-lg text-white">Closed</p>
           </div>
-          <div className="flex w-max flex-col items-center justify-center rounded-lg bg-wolt-blue/[8%] px-2 py-1 text-wolt-blue">
-            <p className="w-max text-sm">{restaurant.venue.estimate_range}</p>
-            <p className="text-xs opacity-[64%]">min</p>
-          </div>
-        </div>
-        <div className="flex h-8 items-center px-4">
-          <p className="text-xs opacity-[64%]">
-            {restaurant.venue.delivery_price}
+        ) : null}
+      </div>
+      <div className="flex w-full justify-between gap-x-4 border-b border-dashed p-4">
+        <div className="flex-col overflow-hidden flex w-0 flex-1">
+          <p className="text-lg font-medium truncate">{restaurant.title}</p>
+          <p className="truncate text-sm opacity-[64%]">
+            {restaurant.venue.short_description}
           </p>
         </div>
+        <div className="flex w-max flex-col items-center justify-center rounded-lg bg-wolt-blue/[8%] px-2 py-1 text-wolt-blue">
+          <p className="w-max text-sm">{restaurant.venue.estimate_range}</p>
+          <p className="text-xs opacity-[64%]">min</p>
+        </div>
+      </div>
+      <div className="flex h-8 items-center px-4">
+        <p className="text-xs opacity-[64%]">
+          {restaurant.venue.delivery_price}
+        </p>
       </div>
     </Link>
   );
